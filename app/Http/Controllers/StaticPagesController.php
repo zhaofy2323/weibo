060c5cab34221abea4f187b1c74e1a342022c8ee
vagrant <vagrant@homestead>
2020-11-09 08:19:35 +0000

    基础页面

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    //
+    public function home(){
+        return view('static_pages/home');
+    }
+
+    public function help(){
+        return view('static_pages/help');
+    }
+
+    public function about(){
+        return view('static_pages/about');
+    }
 }
