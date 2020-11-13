2e048b31e8ecee5fa92434d2c8a789419fb3bf04
zhaofy <zhaofy@steelhome.cn>
2020-11-13 07:44:45 +0000

    更新用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,6 +18,14 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    /**
+     * 用户注册
+     * Created by zfy.
+     * Date:2020/11/13 15:23
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -31,8 +39,42 @@ class UsersController extends Controller
             'email' => $request['email'],
             'password' => bcrypt($request->password),
         ]);
+        //注册成功后自动登录
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    /**
+     * 用户编辑
+     * Created by zfy.
+     * Date:2020/11/13 15:24
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * 修改用户
+     * Created by zfy.
+     * Date:2020/11/13 15:30
+     */
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success', "个人资料更新成功");
+        return redirect()->route('users.show', $user->id);
     }
 }
