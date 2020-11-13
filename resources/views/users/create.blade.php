fa289fd1d236bed73c781ca80d20eb26526610aa
zhaofy <zhaofy@steelhome.cn>
2020-11-13 02:41:14 +0000

    中文错误提示

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,7 @@
                 <h5>注册</h5>
             </div>
             <div class="card-body">
+                @include('shared._errors')
                 <form method="POST" action="{{ route('users.store') }}">
                     {{ csrf_field() }}
 
@@ -28,7 +29,8 @@
 
                     <div class="form-group">
                         <label for="password_confirmation">确认密码：</label>
-                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                        <input type="password" name="password_confirmation" class="form-control"
+                               value="{{ old('password_confirmation') }}">
                     </div>
 
                     <button type="submit" class="btn btn-primary">注册</button>
