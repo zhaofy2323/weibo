218815b14c53c0e0769b4d91ea9ecbc6b1ae8e90
zhaofy <zhaofy@steelhome.cn>
2020-11-14 10:11:35 +0000

    注册页面

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.default')
-@section('title', '登录')
+@section('title', '注册')
 
 @section('content')
     <div class="offset-md-2 col-md-8">
         <div class="card ">
             <div class="card-header">
-                <h5>登录</h5>
+                <h5>注册</h5>
             </div>
             <div class="card-body">
+
                 @include('shared._errors')
 
-                <form method="POST" action="{{ route('login') }}">
+                <form method="POST" action="{{ route('users.store') }}">
                     {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <label for="name">名称：</label>
+                        <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                    </div>
 
                     <div class="form-group">
                         <label for="email">邮箱：</label>
@@ -24,18 +30,12 @@
                     </div>
 
                     <div class="form-group">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">记住我</label>
-                        </div>
+                        <label for="password_confirmation">确认密码：</label>
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
                     </div>
 
-                    <button type="submit" class="btn btn-primary">登录</button>
+                    <button type="submit" class="btn btn-primary">注册</button>
                 </form>
-
-                <hr>
-
-                <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p>
             </div>
         </div>
     </div>
